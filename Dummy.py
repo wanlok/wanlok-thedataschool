b1aa46a9705d9ae5f3fd6bb14fecf9ec14dcb7eb
Robert Wan <wantunglok@gmail.com>
2024-04-03 20:17:02 +1100

    merge two py files

--- Dummy.py
+++ Dummy.py
@@ -197,7 +197,46 @@ def get_empty_players():
 #         print(player)
 
 
+def get_player_clubs():
+    player_dict = dict()
+    with open('data.csv', encoding='utf-8') as csv_file:
+        for row in csv.reader(csv_file, delimiter=','):
+            name = row[0]
+            club_dict = dict()
+            for line in row[1][:-2].split('|'):
+                line = line.replace('\\n', '')
+                slices = line.split('=')
+                if len(slices) > 1:
+                    key = slices[0].strip()
+                    value = slices[1].replace('[[', '').replace(']]', '').replace('→', '').replace('(loan)', '').strip()
+                    if len(value) > 0:
+                        if 'years' in key and 'youthyears' not in key and 'collegeyears' not in key and 'manageryears' not in key and 'coaching_years' not in key and 'serviceyears' not in key:
+                            key = int(key.replace('years', ''))
+                            if key not in club_dict:
+                                club_dict[key] = dict()
+                            club_dict[key]['period'] = value
+                        elif 'clubs' in key and 'youthclubs' not in key and 'managerclubs' not in key:
+                            key = int(key.replace('clubs', ''))
+                            if key not in club_dict:
+                                club_dict[key] = dict()
+                            club_dict[key]['name'] = value
+            clubs = []
+            for i in range(1, len(club_dict) + 1):
+                j = i
+                while j not in club_dict:
+                    j = j + 1
+                club = club_dict[j]
+                if 'name' in club:
+                    clubs.append(club)
+            player_dict[name] = clubs
+    return player_dict
+
+
 if __name__ == '__main__':
+    # my_dict = get_player_clubs()
+    # for key in my_dict:
+    #     print(f'"{key}","{my_dict[key]}"')
+
     year_dict = get_goals_by_years()
     for year in year_dict:
         print(f'{year} {year_dict[year]}')
